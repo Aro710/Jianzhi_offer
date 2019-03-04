6a61898e1a9e7d3d0bbbe1f8ace3dad4d6955f8a
Aro <Aro@ArodeMacBook-Air.local>
2019-03-04 18:37:10 +0800

    Mar.4

--- 数组中出现次数超过一半的数字.cpp
+++ 数组中出现次数超过一半的数字.cpp
@@ -9,15 +9,28 @@ public:
         if(count >numbers.size()/2) return numbers[numbers.size()/2];
         else return 0;
     }
-    
+    //利用count来计数，超过一半的数，一定比其他所有数加起来出现的次数还多
+    //count == 0 result 就等于当前数
     int MoreThanHalfNum_Solution(vector<int> numbers){
         int size = numbers.size();
-        int* num =new int[size+1];
-        memset(num,0,size+1);
-        for(int i = 0; i <size;i++){
-            num[numbers[i]]++;
-            if(num[numbers[i]]>size/2) return numbers[i];
+        if(size == 0) return 0;
+        int result = numbers[0];
+        int count = 1;
+        for(int i =0 ;i<size;i++){
+            if(count == 0){
+                result = numbers[i];
+                count++;
+            }
+            else if(result == numbers[i])
+                count++;
+            else 
+                count--;
         }
-        return 0;
+        int resCount =0;
+        for(int i =0 ; i<size;i++){
+            if(numbers[i] == result) resCount++;
+        }
+        if(resCount>size/2) return result;
+        else return 0;
     }
 };
